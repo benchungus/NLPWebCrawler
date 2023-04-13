11861b15bf82a03270e0686367939cced1bd71d7
benchungus <90946677+benchungus@users.noreply.github.com>
2023-04-13 02:28:15 -0500

    wiki webscraper

---
+++ despmecrawler.py
@@ -0,0 +1,60 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+from collections import deque
+from urllib import request
+
+
+url = "https://despicableme.fandom.com/wiki/Category:Characters"
+
+response = requests.get(url)
+
+soup = BeautifulSoup(response.content, "html.parser")
+
+member_links = soup.find_all("a", class_="category-page__member-link")
+
+member_links_filtered = [link for link in member_links if "Category" not in link["href"]]
+
+href_strings = []
+for link in member_links_filtered:
+    href_strings.append(str(link.get('href')))
+
+prefix = "https://despicableme.fandom.com/"
+
+absolute_links = [prefix + link for link in href_strings]
+
+for link in absolute_links:
+    print(link)
+
+
+
+siteQueue = deque()
+
+for link in absolute_links:
+    siteQueue.append(link)
+
+visited = set()
+
+while len(siteQueue) > 0:
+    url = siteQueue.pop()
+
+    response = requests.get(url)
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    paragraph = None
+
+    for p in soup.find_all('p'):
+        if p.find('b') is not None:
+            all_text = p.text.strip()
+            if all_text.count('\n') <= 1:
+                paragraph = p
+                break
+
+    if paragraph is not None:
+        bold_text = paragraph.find('b').text
+        all_text = paragraph.text.strip()
+        print('First bold text:', bold_text)
+        print('All text:', all_text)
+    else:
+        print('No paragraph found with bold text')
--- web_crawler.py
+++ web_crawler.py
@@ -1,62 +1,18 @@
 from bs4 import BeautifulSoup
 import requests
-from collections import deque
-from urllib import request
 
-def webScrape(starter_url, keyword1, keyword2): 
+# Replace the URL with the actual URL of the category page you want to scrape
+url = "https://despicableme.fandom.com/wiki/Category:Characters"
 
-    siteQueue = deque([starter_url])
+# Send a request to the URL and get the HTML response
+response = requests.get(url)
 
-    visited = set()
+# Parse the HTML response using Beautiful Soup
+soup = BeautifulSoup(response.content, "html.parser")
 
-    with open('urls.txt', 'w') as f:
-        counter = 0
-        while counter < 30:
-            currUrl = siteQueue.pop()
-            visited.add(currUrl)
-            r = requests.get(currUrl)
-            data = r.text
-            soup = BeautifulSoup(data, features="html.parser")
-    # write urls to a file
-            for currUrl in soup.find_all('a'):
-                link_str = str(currUrl.get('href'))
-                ##print(link_str)
-                if keyword1 in link_str or keyword2 in link_str:
-                    if link_str.startswith('/url?q='):
-                        link_str = link_str[7:]
-                        ##print('MOD:', link_str)
-                    if '&' in link_str:
-                        i = link_str.find('&')
-                        link_str = link_str[:i]
-                    if link_str.startswith('http') and 'google' not in link_str:
-                        siteQueue.append(link_str)
-                        f.write(link_str + '\n')
-                        filename = f"{counter}textfile.txt"
-                        with open(filename, 'w', encoding='utf-8') as f1:
-                            try:
-                                errResp = requests.get(link_str)
-                                if errResp.status_code == 200:     
-                                    with request.urlopen(link_str) as f2: 
-                                        raw = f2.read().decode('utf-8-sig')
-                                        soup2  = BeautifulSoup(raw, features="html.parser")
-                                        p_tags = soup2.find_all('p')
-                                        if(len(p_tags) > 0):
-                                            for p in p_tags:
-                                                f1.write(p.text)
-                                                f1.write('\n')
-                                            counter += 1
-                                            if counter >= 30:
-                                                break
-                                else:
-                                    print("http error")        
-                            except requests.exceptions.HTTPError as error:
-                                print("error: " + error)
-                            except requests.exceptions.ConnectionError as error:
-                                print("error: " + error)
-    # end of program
-    print("end of crawler")
-            
+# Find all the category page member links
+member_links = soup.find_all("a", class_="category-page__member-link")
 
-starter_url = "https://en.wikipedia.org/wiki/Despicable_Me"
-webScrape(starter_url, "Despicable", "despicable")
-##textScraper("urls.txt")
+# Print the member links
+for link in member_links:
+    print(link["href"])
